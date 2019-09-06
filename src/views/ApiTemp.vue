e6e07854a6f3d331c26285d5ac05a41d7989abda
flydeagle <flydeagle@hotmai.com>
2019-09-06 23:01:25 +0800

    kkk

---
+++ src/views/ApiTemp.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="page api_temp">
+        wwwwwwww
+    </div>
+</template>
+
+<script>
+export default {
+    name: "apiTemp" ,
+    date() {
+        return{
+            tableData: []
+        }
+    },
+
+    created() {
+        this.getData()
+    },
+
+    methods:{
+        async getData() {
+            const source = 'http://localhost:3000/tableData'
+            let res = await this.$api.get(source)
+            this.tableData = [...res]
+            console.log(res)
+        },
+    }
+}
+</script>
